e347b7e671c0258b3e66f84d29d8d0fc85a75926
Fabian Jocks <24557998+iamfj@users.noreply.github.com>
2026-02-04 20:11:39 +0100

    refactor: create shared type aliases from codegen types

---
+++ src/common/types.ts
@@ -0,0 +1,42 @@
+import type {
+  GetIssuesQuery,
+  GetIssueByIdQuery,
+  GetIssueByIdentifierQuery,
+  SearchIssuesQuery,
+  CreateIssueMutation,
+  UpdateIssueMutation,
+  GetDocumentQuery,
+  ListDocumentsQuery,
+  DocumentCreateMutation,
+  DocumentUpdateMutation,
+  ListAttachmentsQuery,
+  AttachmentCreateMutation,
+  GetProjectMilestoneByIdQuery,
+  ListProjectMilestonesQuery,
+  CreateProjectMilestoneMutation,
+  UpdateProjectMilestoneMutation,
+} from "../gql/graphql.js";
+
+// Issue types
+export type Issue = GetIssuesQuery["issues"]["nodes"][0];
+export type IssueDetail = NonNullable<GetIssueByIdQuery["issue"]>;
+export type IssueByIdentifier = GetIssueByIdentifierQuery["issues"]["nodes"][0];
+export type IssueSearchResult = SearchIssuesQuery["searchIssues"]["nodes"][0];
+export type CreatedIssue = NonNullable<CreateIssueMutation["issueCreate"]["issue"]>;
+export type UpdatedIssue = NonNullable<UpdateIssueMutation["issueUpdate"]["issue"]>;
+
+// Document types
+export type Document = NonNullable<GetDocumentQuery["document"]>;
+export type DocumentListItem = ListDocumentsQuery["documents"]["nodes"][0];
+export type CreatedDocument = DocumentCreateMutation["documentCreate"]["document"];
+export type UpdatedDocument = DocumentUpdateMutation["documentUpdate"]["document"];
+
+// Attachment types
+export type Attachment = ListAttachmentsQuery["issue"]["attachments"]["nodes"][0];
+export type CreatedAttachment = AttachmentCreateMutation["attachmentCreate"]["attachment"];
+
+// Milestone types
+export type MilestoneDetail = NonNullable<GetProjectMilestoneByIdQuery["projectMilestone"]>;
+export type MilestoneListItem = ListProjectMilestonesQuery["project"]["projectMilestones"]["nodes"][0];
+export type CreatedMilestone = NonNullable<CreateProjectMilestoneMutation["projectMilestoneCreate"]["projectMilestone"]>;
+export type UpdatedMilestone = NonNullable<UpdateProjectMilestoneMutation["projectMilestoneUpdate"]["projectMilestone"]>;
